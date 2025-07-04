4b954a3b135a501a51242baf8e7be7440b3b1b45
sirtrainsalot <sir.trainsalot@gmail.com>
2025-07-04 09:04:29 +0200

    add message for select-difficulty and fix welcome-message

--- src/number_guessing_game/core.clj
+++ src/number_guessing_game/core.clj
@@ -2,13 +2,19 @@
   (:gen-class))
 
 (defn welcome-message []
-  (println "hei"))
+  (println "Welcome to the Number Guessing Game!\nI'm thinking of a number between 1 and 100.\nYou have to guess the correct number.\n")
+  )
+
+(defn select-difficulty []
+  (println "Please select the difficulty level:\n1. Easy (10 chances)\n2. Medium (5 chances)\n3. Hard (3 chances)\n"))
+
 
 (defn -main [& args]
   ;; print welcome message
   (welcome-message)
   ;; select random number
   ;; user input - select difficulty
+  (select-difficulty)
   ;; game loop:
   ;;   Enter your guess: <user input>
   ;;   wrong:
