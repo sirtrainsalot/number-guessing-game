dfe95c9f58eb3aebb2da6084d7ab4bdfa44bf8d5
sirtrainsalot <sir.trainsalot@gmail.com>
2025-07-04 10:54:32 +0200

    add game loop and clean up main function

--- src/number_guessing_game/core.clj
+++ src/number_guessing_game/core.clj
@@ -19,19 +19,41 @@
 (defn get-secret-number []
   (+ (rand-int 100) 1))
 
+(defn parse-int [s]
+  (try
+    (Integer/parseInt s)
+    (catch Exception _ nil)))                               ;; handles error input
+
+(defn game-loop [secret remaining attempts]
+  (if (zero? remaining)
+    (println (str "You're out of guesses. The number I was thinking about was " secret))
+    (do
+      (print "Enter your guess: ")
+      (flush)
+      (let [guess (parse-int (read-line))]
+        (if (or (nil? guess) (< guess 1) (> guess 100))
+          (do
+            (println "Invalid input. Enter a number between 1 and 100")
+            (recur secret remaining attempts))
+          (cond
+            (= guess secret)
+            (println (str "Congratulations! You guessed the correct number in " attempts " attempts."))
+
+            (< guess secret)
+            (do
+              (println "Incorrect! The number is greater than" guess ".\n")
+              (recur secret (dec remaining) (inc attempts)))
+
+            (> guess secret)
+            (do
+              (println "Incorrect! The number is less than" guess ".\n")
+              (recur secret (dec remaining) (inc attempts)))))))))
+
+
 (defn -main [& args]
   (welcome-message)
   (let [{:keys [level chances]} (select-difficulty)
-        secret-number (get-secret-number)]
-    (println "Great! You've choosen difficulty level:" (name level))
-    (println "You have" chances "chances. Good luck!")
-    )
-
-  ;; game loop:
-  ;;   Enter your guess: <user input>
-  ;;   wrong:
-  ;;     - print less or more
-  ;;     - add to nrOfGuesses
-  ;;   correct
-  ;;     - print congrats + nrOfGuesses
-  )
+        secret (get-secret-number)]
+    (println "Great! You have selected the" (name level) "difficulty level.")
+    (println "You have" chances "chances.\nLet's start the game!\n")
+    (game-loop secret chances 0)))
